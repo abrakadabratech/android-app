e170844f56778efdebafeeb3a1c37778b864984a
Venkat <venkat.pv71@gmail.com>
2024-04-08 09:05:26 +0530

    added app version in headers

--- app/src/main/java/com/oss/abraakadabraaapp/retrofit/api/TokenInterceptor.kt
+++ app/src/main/java/com/oss/abraakadabraaapp/retrofit/api/TokenInterceptor.kt
@@ -1,15 +1,12 @@
 package com.oss.abraakadabraaapp.retrofit.api
 
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.util.Log
-import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GetTokenResult
+import com.oss.abraakadabraaapp.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.util.concurrent.TimeUnit
@@ -28,10 +25,12 @@ class TokenInterceptor() : Interceptor {
             val task: Task<GetTokenResult> = user.getIdToken(false)
             val tokenResult = Tasks.await(task, 20, TimeUnit.SECONDS)
             val token = tokenResult.token
+            val version = BuildConfig.VERSION_CODE
 
-            Log.w("OkHttp", "intercept Token: $token", )
-            request = request.newBuilder().addHeader("Authorization", "Bearer $token").build()
-
+            Log.w("OkHttp", "intercept Token: $token apk version : $version", )
+            request = request.newBuilder()
+                .header("androidversion", version.toString())
+                .addHeader("Authorization", "Bearer $token").build()
             return chain.proceed(request)
         } catch (e: Exception) {
             return chain.proceed(request)
